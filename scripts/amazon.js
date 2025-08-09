e40168ad12c808cb70bf07eb2f85fcbf2a614b05
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-08-09 22:06:19 +0600

    Use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,41 +1,3 @@
-const products = [
-  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-    keywords: ["socks", "sports", "apparel"],
-  },
-  {
-    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-    keywords: ["sports", "basketballs"],
-  },
-  {
-    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-    keywords: ["tshirts", "apparel", "mens"],
-    type: "clothing",
-    sizeChartLink: "images/clothing-size-chart.png",
-  },
-];
-
 let productHtml = '';
 products.forEach((product) => {
   productHtml += `
@@ -89,6 +51,5 @@ products.forEach((product) => {
         </div>`;
 });
 
-console.log(productHtml)
 
 document.querySelector('.js-products-grid').innerHTML =  productHtml
